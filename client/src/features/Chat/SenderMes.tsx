771c10617b17fbbc745a7dc1932e864d8870521e
maksimkrylov <150614120+maksimkrylov@users.noreply.github.com>
2024-02-15 15:19:29 +0300

    1

--- client/src/features/Chat/SenderMes.tsx
+++ client/src/features/Chat/SenderMes.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import {type Message } from './types';
+import { type Message } from './types';
 import { type User } from '../Page/SignPage/types';
 
-function SenderMes({message, user}: {message: Message, user: User}):JSX.Element {
-  
+function SenderMes({ message, user }: { message: Message; user: User }): JSX.Element {
   return (
     <li className="slds-chat-listitem slds-chat-listitem_outbound">
       <div className="slds-chat-message-outbound">
@@ -11,7 +10,7 @@ function SenderMes({message, user}: {message: Message, user: User}):JSX.Element 
           <div className="slds-chat-message__text slds-chat-message__text_outbound">
             <span>{message.content}</span>
           </div>
-          <div className="slds-chat-message__meta" >
+          <div className="slds-chat-message__meta">
             {user?.name} • {`${message.createdAt.slice(0, 10)} ${message.createdAt.slice(11, 19)}`}
           </div>
         </div>
